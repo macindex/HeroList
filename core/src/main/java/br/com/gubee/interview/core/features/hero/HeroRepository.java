b1862c5c8ce1a4cfadece2d08fd73b780d2eb3b3
macindex <machollanda@gmail.com>
2023-04-01 09:13:09 -0300

    ajustes controller repo e service

--- core/src/main/java/br/com/gubee/interview/core/features/hero/HeroRepository.java
+++ core/src/main/java/br/com/gubee/interview/core/features/hero/HeroRepository.java
@@ -1,4 +1,16 @@
 package br.com.gubee.interview.core.features.hero;
 
-public class HeroRepository {
+import br.com.gubee.interview.model.Hero;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface HeroRepository extends JpaRepository<Hero, UUID> {
+    // @Query("")
+    // custom composite repository here
 }
--- core/src/main/java/br/com/gubee/interview/core/features/hero/HeroService.java
+++ core/src/main/java/br/com/gubee/interview/core/features/hero/HeroService.java
@@ -1,4 +1,48 @@
 package br.com.gubee.interview.core.features.hero;
 
+import br.com.gubee.interview.model.Hero;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public class HeroService {
+    private final HeroRepository heroRepository;
+
+    @Autowired
+    public HeroService(HeroRepository heroRepository) {
+        this.heroRepository = heroRepository;
+    }
+
+    public List<Hero> findAllHeroes() {
+        return heroRepository.findAll();
+    }
+
+    public Optional<Hero> findHeroById(UUID id) {
+        findOrThrow(id);
+        return heroRepository.findById(id);
+    }
+
+    public void removeHeroById(UUID id) {
+        findOrThrow(id);
+        heroRepository.deleteById(id);
+    }
+
+    public Hero addHero(Hero hero) {
+        return heroRepository.save(hero);
+    }
+
+    public void updateHero(UUID id, Hero hero) {
+        findOrThrow(id);
+        heroRepository.save(hero);
+    }
+
+    private void findOrThrow(final UUID id) {
+        heroRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("404 id not found " + id)
+                );
+    }
 }
